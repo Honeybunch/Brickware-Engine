5cd4534cefbc5b93b661f0e43f650ae0f86a3558
Honeybunch <amt3824@g.rit.edu>
2015-10-06 00:44:20 -0400

    Fixed warnings on Linux. Moved shader version to lowest required

--- BrickwareGraphics/src/GLDebugCallback.cpp
+++ BrickwareGraphics/src/GLDebugCallback.cpp
@@ -13,9 +13,9 @@ extern "C" {
 		GLenum severity, GLsizei length,
 		const GLchar *msg, const void *data)
 	{
-		char* _source;
-		char* _type;
-		char* _severity;
+		const char* _source;
+		const char* _type;
+		const char* _severity;
 
 		switch (source) {
 		case GL_DEBUG_SOURCE_API:
@@ -109,7 +109,7 @@ extern "C" {
 				id, _type, _severity, _source, msg);
 			int i = 0;
 		}
-		
+
 	}
 
 #ifdef __cplusplus
--- Shaders/PointShadowPixel.glsl
+++ Shaders/PointShadowPixel.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 150 core
 
 in vec4 fragPos;
 

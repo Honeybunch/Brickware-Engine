8a43aafb8411e287876b3a95d43641f3339af825
Honeybunch <amt3824@g.rit.edu>
2015-12-03 01:05:32 -0500

    Fixed fullscreen triangle
    Camera controls are fucked but it kinda works

--- Shaders/ScreenPixel.glsl
+++ Shaders/ScreenPixel.glsl
@@ -1,8 +1,11 @@
 #version 130
 
+uniform sampler2D frame;
+
+in vec2 uv;
 out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4(1, 0, 0, 1);
+	fragColor = texture(frame, uv);
 }
--- Shaders/ScreenVertex.glsl
+++ Shaders/ScreenVertex.glsl
@@ -7,5 +7,5 @@ void main()
 	uv.x = (gl_VertexID == 2) ? 2.0 : 0.0;
 	uv.y = (gl_VertexID == 1) ? 2.0 : 0.0;
 
-	gl_Position = vec4(uv * vec2(2.0, -2.0) + vec2(-1, 1), 1.0, 1.0);
+	gl_Position = vec4(uv * vec2(-2.0, 2.0) + vec2(1, -1), 0, 1.0);
 }
